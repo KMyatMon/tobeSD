8b761ee72811e0bf1876fc9a24ebc6920fd1ab76
Khin Myat Mon <58884581+KMyatMon@users.noreply.github.com>
2020-06-21 19:05:13 +0630

    Add files via upload

---
+++ week 3 node_js/reference/os_demo.js
@@ -0,0 +1,25 @@
+// inf about file operationg system
+
+const os =require('os');
+
+//  platform 
+console.log(os.platform());
+
+//  CPU  architecture
+console.log(os.arch());
+
+//  CPU Core Info
+console.log(os.cpus());
+
+//free memory
+console.log(os.freemem());
+
+//total memo
+console.log(os.totalmem());
+
+// home dir
+console.log(os.homedir());
+
+//  Uptime
+console.log(os.uptime());
+ 
